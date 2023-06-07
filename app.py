15e023514470c68f740ae4881e858990b6310c18
Akshit Srivastava <88588444+restlesshornet@users.noreply.github.com>
2023-06-07 20:17:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,18 +67,18 @@ def main():
     
     st.image("logo.png")
     ##MainMenu
-    st.sidebar.header("Navigation")
+    
     with st.sidebar:
-        selected = st.sidebar.selectbox(
-        "Main Menu",
-        options=[
-            "Project Information",
-            "Model Information",           
-            "Predict Defects",
-            "Contributors"
-        ],
-
-    )
+        selected = option_menu.option_menu(
+            "Main Menu",
+            options=[
+                "Project Information",
+                "Model Information",           
+                "Predict Defects",
+                "Contributors"
+            ],
+        )
+    
     st.sidebar.markdown('---')
         
     ##HOME page 
@@ -165,7 +165,11 @@ def main():
 
             
     elif selected == "Contributors":
-        st.text("List of Contributors")
+        st.subheader("Contributors")
+        st.markdown("<b><u>Project Leads :</u></b> Eeman Majumder, Vaasu Bisht , Cesar Tinoco & Mario Rodriguez", unsafe_allow_html=True)
+        st.markdown("<b><u>Management & HR:</u><b> Sakshi Sawarkar ", unsafe_allow_html=True)
+        st.markdown("<b><u>Project Contributors :</u></b> \n  ", unsafe_allow_html=True)
+        st.text(""" 1.  Adeeba Rashid \n 2.  Aditya Narayan Jha \n 3.  Akshit Srivastava \n 4.  Ameya Sharma \n 5.  Ananya Tiwari \n 6.  Annirudha Kumar \n 7.  Arjita Arora \n 8.  Ashwin J R \n 9.  Bhushan Kumar \n 10. Darshnik Rohal \n 11. Debadrita Dey \n 12. Devanshi Pathak \n 13. Enrique Unzueta \n 14. Jyotsna Bhatia \n 15. Mohit Kumar Saw \n 16. Mrunmayee Ketkar \n 17. Mudit Gaur \n 18. Navneet Lamba \n 19. Punit Kaushik \n 20. Pushpendra Kushwaha \n 21. Qurat ul aaein \n 22. Ram Vikram Singh \n 23. Sergio Reyes \n 24. Shreya Tripathi \n 25. Simone Reynoso \n 26. Soumyashis Sarkar \n 27. Sourav Dutta \n 28. Suhani Thakur \n 29. Swetha Thampi M \n 30. Tarandeep Singh Juneja \n 31. Yahya Ismaiel \n""" )
 
     elif selected == "Model Information":
         st.subheader('Introduction')
