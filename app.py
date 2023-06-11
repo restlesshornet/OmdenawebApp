78f79ba1efe0be3e16d1929a093be7dad7fc77b4
Akshit Srivastava <88588444+restlesshornet@users.noreply.github.com>
2023-06-11 19:02:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,12 +15,6 @@ from ultralytics.yolo.utils.plotting import Annotator
 from cv2 import cvtColor
 import os
 
-with open('packages.txt', 'r') as f:
-    packages = f.readlines()
-for package in packages:
-    package = package.strip()  # Remove leading/trailing whitespaces
-    command = f"sudo apt-get --allow-releaseinfo-change-suite install -y {package}"
-    os.system(command)
 
 model = YOLO('best.pt')
 def bgr2rgb(image):
