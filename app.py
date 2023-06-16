eac3cdf34c6fd7542658fa6d1ed504fce1520344
Akshit Srivastava <88588444+restlesshornet@users.noreply.github.com>
2023-06-16 17:37:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,8 +103,12 @@ def main():
         impact economic efficiency. To maintain roads as efficiently as possible, municipalities perform regular
         inspections. The aim of the project is to use machine learning to study and analyze different types of road defects
         and to automatically detect any road abnormalities.
+        """
         
-        The goal of this project is to design, build and test an inspection system for detecting road abnormalities, defects, and damages
+        st.text(problem_statement)
+        
+        with st.expander("Read More"): 
+            text = """The goal of this project is to design, build and test an inspection system for detecting road abnormalities, defects, and damages
         using machine learning. The proposed system aims to improve the efficiency of road inspections and reduce
         the time and labor required for the process. The system will be equipped with a camera to capture video streams
         from different roads, and the data will be analyzed using the Matlab machine learning toolbox to train and test the network.
@@ -112,10 +116,10 @@ def main():
         The approach will involve three main tasks: data acquisition, data training/testing, and dashboard building and testing. 
         Ultimately, the proposed system will help to maintain roads more efficiently, enhance driver comfort, and improve economic 
         efficiency. Additionally, the system will provide insights into the causes of road abnormalities in Indian roads, 
-        including pitfalls, sinks, flooding, and traffic congestion due to insufficient lanes in cities and towns.
-        """
+        including pitfalls, sinks, flooding, and traffic congestion due to insufficient lanes in cities and towns."""
+            
+            st.text(text)
         
-        st.text(problem_statement)
         st.subheader("Our Solution")
         Project_goal = """
         Our Team developed a Machine Learning ( ML ) model based on the YOLOv8 Architecture, which was trained on a comprehensive
